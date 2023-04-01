a4cba8fc80a73ec1d8b10cc3e54b27a0d2cf5ab0
Onesmus Wambugu <onesmus1024@gmail.com>
2023-04-01 20:16:40 +0300

    functionality: added Thunk to handle side effects

--- src/redux/reducers/courseReuducers.js
+++ src/redux/reducers/courseReuducers.js
@@ -1,9 +1,11 @@
-
+import * as types from "../../constants/actionTypes";
 
 export function courseReducer(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case types.CREATE_COURSE:
       return [...state, { ...action.course }];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
